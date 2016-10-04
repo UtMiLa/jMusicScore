e78db4a73245b742d84683196058bdc1d92b4ea1
JohnEkberg <joek@adm.ku.dk>
2016-10-04 16:07:36 +0200

    browserFileSystem til japps

--- jMusicScore/References.ts
+++ jMusicScore/References.ts
@@ -1,3 +1,4 @@
 ﻿/// <reference path="node_modules/japps/application.d.ts" />
 /// <reference path="node_modules/japps/jApps.ui.d.ts" />
 /// <reference path="node_modules/japps/keyboard.d.ts" />
+/// <reference path="node_modules/japps/jApps.BrowserFileSystem.d.ts" />
