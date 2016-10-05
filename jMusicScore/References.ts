85254f91ad474f46f80adb66740167444fc9e464
JohnEkberg <joek@adm.ku.dk>
2016-10-05 16:17:38 +0200

    fix dependies

--- jMusicScore/References.ts
+++ jMusicScore/References.ts
@@ -1,7 +1,4 @@
-﻿/// <reference path="node_modules/japps/application.d.ts" />
-/// <reference path="node_modules/japps/jApps.ui.d.ts" />
-/// <reference path="node_modules/japps/keyboard.d.ts" />
-/// <reference path="node_modules/japps/jApps.BrowserFileSystem.d.ts" />
+﻿/// <reference path="node_modules/japps/jApps.d.ts" />
 declare namespace JQueryUI {
     interface ButtonOptions {
         icon?: string;
