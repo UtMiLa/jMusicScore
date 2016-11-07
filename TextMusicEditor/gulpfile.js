ff92a7aaaa7fc10939be80f2e81ca7c0d4a3cbc8
JohnEkberg <joek@adm.ku.dk>
2016-11-07 16:19:09 +0100

    test

--- TextMusicEditor/gulpfile.js
+++ TextMusicEditor/gulpfile.js
@@ -1,5 +1,7 @@
 ﻿var gulp = require('gulp');
+var peg = require("pegjs");
 
 gulp.task('default', function () {
     // place code for your default task here 
+    var parser = peg.generate("start = ('a' / 'b')+");
 });
