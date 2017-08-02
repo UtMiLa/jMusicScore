d531dfd7c0bee20bb5a652d9c584892058ae2e7a
John Erik Rønne Ekberg <rnj283@ku.dk>
2017-08-02 10:00:41 +0200

    flyt peg til fil

--- noteEditor/ScriptRunner.ts
+++ noteEditor/ScriptRunner.ts
@@ -46,59 +46,6 @@ Strukturer:
   
 */
 
-/*
-
-Statement
-  = Assignment
-  / Command
-
-Assignment
-  = v:VariableName _"=" _ e:Command { return { type: "assignment", v: v, e: e } }
-
-Command
-  = c:CommandName _ args:Arg* { return { type: "command", c:c, args:args }}
-
-VariableName
-  = Identifier
-
-CommandName
-  = Identifier
-
-Identifier
-  = _ id:[A-Za-z]+ { return id.join(''); }
-
-Arg
-  = t:String _ {return {type: "string", val: t}}
-  / r:Rational _ {return {type: "rational", val: r}}
-  / i:Integer _ {return {type: "number", val: i}}
-  / "<" _ m:Music* _ ">"{return {type: "music", val: m}}
-  / v:Voice _ {return {type: "voice", val: v}}
-  / s:Staff _ {return {type: "staff", val: s}}
-  / v:Identifier _ {return {type: "identifier", val: v }; }
-  / v:[^ ]+ _ {return {type: "expression", val: v.join('') }; }
-
-Voice
-  = [s]s:[0-9]+[v]v:[0-9]+ _ {return {s:s.join(''),v:v.join('')}}
-
-Staff
-  = [s]s:[0-9]+ _ {return {s:s.join('')}}
-
-Rational
-  = i1:Integer "/" i2:Integer { return { num: i1, den: i2};  }
-
-String
-  = _ '"' t:([^"]*) '"' {return t.join(""); }
-
-Music
-  = "c"
-
-Integer "integer"
-  = _ [0-9]+ { return parseInt(text(), 10); }
-
-_ "whitespace"
-  = [ \t\n\r]*
-
- */
 
 namespace ScriptRunner {
 
