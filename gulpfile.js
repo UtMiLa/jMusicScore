e25a207e55e4614df4bceba9ae4bc0c55efac872
John Erik Rønne Ekberg <rnj283@ku.dk>
2017-08-04 14:54:03 +0200

    gulpfile for whole solution

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,58 @@
-// For more information on how to configure a task runner, please visit:
-// https://github.com/gulpjs/gulp
+/// <binding BeforeBuild='default1' />
+var gulp = require('gulp');
+//var pegjs = require("gulp-pegjs");
 
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gutil = require('gulp-util');
+var through = require('through2');
+var assign = require('object-assign');
 
-gulp.task('default', function() {
-    gutil.log('Gulp.js has been successfully installed!\n' +
-              'For more information on how to configure it, please visit:\n' +
-              'https://github.com/gulpjs/gulp');
+var pegjs = require('pegjs');
+var ts = require("gulp-typescript");
+
+var pegjsfunc = function (opts) {
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        } 
+
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-pegjs', 'Streaming not supported'));
+            return;
+        }
+
+        // always generate source code of parser
+        var options = assign({ output: 'source' }, opts);
+
+        var filePath = file.path;
+
+
+        try {
+            file.contents = new Buffer(pegjs.generate(file.contents.toString(), options));
+            file.path = gutil.replaceExtension(file.path, '.js');
+            this.push(file);
+        } catch (err) {
+            this.emit('error', new gutil.PluginError('gulp-pegjs', err, { fileName: filePath }));
+        }
+        cb();
+    });
+};
+
+var txProj = ts.createProject("TextMusicEditor/tsconfig.json", { outFile: "TextMusicEditor/app.js" });
+
+gulp.task('TextMusicEditor', function () {
+    return gulp.src(["TextMusicEditor/app.ts", "TextMusicEditor/node_modules/jmusicscore/*.d.ts", "TextMusicEditor/scripts/typings/**/*.d.ts"])
+        .pipe(txProj())
+        .pipe(gulp.dest("dist/TextMusicEditor"));
 });
+
+
+gulp.task('peg', function () {
+    // place code for your default task here 
+    return gulp.src(['*/*.pegjs'])
+        .pipe(pegjsfunc({ output: "source", trace: true, exportVar: "pegjs", format: "umd" }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('textmusic', ['peg', 'TextMusicEditor']);
+
