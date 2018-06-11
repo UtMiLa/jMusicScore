5e232a07ae5ef4152917fd48725719a4766661d6
Unknown <joek@adm.ku.dk>
2018-06-11 13:28:33 +0200

    linting

--- composer-tool/src/app/jmusic-score-view/jmusic-score-view.component.ts
+++ composer-tool/src/app/jmusic-score-view/jmusic-score-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
-import { MusicElementFactory, IScore, IMemento, ScoreElement, ClefDefinition, NoteType, AbsoluteTime, 
+import { MusicElementFactory, IScore, IMemento, ScoreElement, ClefDefinition, NoteType, AbsoluteTime,
     TimeSpan, Music } from '../../../../jMusic/jm-model';
 // import { Model } from '../../../../jMusicScore/jMusicScore';
 // import { CanvasView } from '../../../../jMusicScore/jMusicScore.CanvasView';
@@ -36,35 +36,44 @@ export class JmusicScoreViewComponent implements OnInit {
                 {
                     "id": "99", "t": "Voice", "def": { "stem": 1 },
                     "children": [
-                        { "id": "100", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 0, "den": 1 }, "noteId": "n1_8", "dots": 1, "rest": true } },
+                        { "id": "100", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 0, "den": 1 },
+                        "noteId": "n1_8", "dots": 1, "rest": true } },
                         {
-                            "id": "101", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 3, "den": 16 }, "noteId": "n1_16" }, "children": [{ "id": "102", "t": "Notehead", "def": { "p": 2, "a": "" } },
+                            "id": "101", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 3, "den": 16 },
+                             "noteId": "n1_16" }, "children": [{ "id": "102", "t": "Notehead", "def": { "p": 2, "a": "" } },
                                 { "id": "156", "t": "TextSyllable", "def": { "text": "tænk " } },
                                 { "id": "159", "t": "NoteLongDecoration", "def": { "type": 3, "dur": { "num": 5, "den": 16 } } },
                                 { "id": "166", "t": "NoteLongDecoration", "def": { "type": 1, "dur": { "num": 5, "den": 16 } } }]
                         },
-                        { "id": "103", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 1, "den": 4 }, "noteId": "n1_8" }, "children": [{ "id": "104", "t": "Notehead", "def": { "p": 4, "a": "" } }, { "id": "157", "t": "TextSyllable", "def": { "text": "på " } }] },
-                        { "id": "105", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 3, "den": 8 }, "noteId": "n1_8" }, "children": [{ "id": "106", "t": "Notehead", "def": { "p": 6, "a": "" } }, { "id": "158", "t": "TextSyllable", "def": { "text": "teks-" } }] },
-                        { "id": "107", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 1, "den": 2 }, "noteId": "n1_8", "grace": "normal" }, "children": [{ "id": "108", "t": "Notehead", "def": { "p": 5, "a": "" } }, { "id": "162", "t": "TextSyllable", "def": { "text": "ten" } }] },
+                        { "id": "103", "t": "Note", "def": { "time": { "num": 1, "den": 8 },
+                        "abs": { "num": 1, "den": 4 }, "noteId": "n1_8" },
+                         "children": [{ "id": "104", "t": "Notehead", "def": { "p": 4, "a": "" } },
+                          { "id": "157", "t": "TextSyllable", "def": { "text": "på " } }] },
+                        { "id": "105", "t": "Note", "def": { "time": { "num": 1, "den": 8 },
+                        "abs": { "num": 3, "den": 8 }, "noteId": "n1_8" }, "children": [
+                            { "id": "106", "t": "Notehead", "def": { "p": 6, "a": "" } },
+                             { "id": "158", "t": "TextSyllable", "def": { "text": "teks-" } }] },
+                        { "id": "107", "t": "Note", "def": { "time": { "num": 1, "den": 8 },
+                        "abs": { "num": 1, "den": 2 }, "noteId": "n1_8", "grace": "normal" }, "children": [
+                            { "id": "108", "t": "Notehead", "def": { "p": 5, "a": "" } },
+                            { "id": "162", "t": "TextSyllable", "def": { "text": "ten" } }] },
                         {
-                            "id": "109", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 1, "den": 2 }, "noteId": "n1_16" }, "children": [{ "id": "110", "t": "Notehead", "def": { "p": 4, "a": "" } }, { "id": "111", "t": "Notehead", "def": { "p": 6, "a": "" } },
+                            "id": "109", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs":
+                            { "num": 1, "den": 2 }, "noteId": "n1_16" }, "children": [
+                                { "id": "110", "t": "Notehead", "def": { "p": 4, "a": "" } },
+                                { "id": "111", "t": "Notehead", "def": { "p": 6, "a": "" } },
                                 { "id": "167", "t": "NoteLongDecoration", "def": { "type": 2, "dur": { "num": 3, "den": 16 } } }]
                         },
-                        { "id": "112", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 9, "den": 16 }, "noteId": "n1_16" }, "children": [{ "id": "113", "t": "Notehead", "def": { "p": 2, "a": "", "tie": true } }, { "id": "114", "t": "Notehead", "def": { "p": 5, "a": "" } }] },
-                        { "id": "115", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 5, "den": 8 }, "noteId": "n1_16" }, "children": [{ "id": "116", "t": "Notehead", "def": { "p": 2, "a": "" } }, { "id": "117", "t": "Notehead", "def": { "p": 4, "a": "" } }] },
-                        { "id": "118", "t": "Note", "def": { "time": { "num": 1, "den": 16 }, "abs": { "num": 11, "den": 16 }, "noteId": "n1_16" }, "children": [{ "id": "119", "t": "Notehead", "def": { "p": 1, "a": "" } }, { "id": "120", "t": "Notehead", "def": { "p": 3, "a": "" } }] },
-                        { "id": "121", "t": "Note", "def": { "time": { "num": 1, "den": 8 }, "abs": { "num": 3, "den": 4 }, "noteId": "n1_8", "dots": 2 }, "children": [{ "id": "122", "t": "Notehead", "def": { "p": 2, "a": "" } }] },
-                        { "id": "123", "t": "Note", "def": { "time": { "num": 1, "den": 32 }, "abs": { "num": 31, "den": 32 }, "noteId": "n1_32" }, "children": [{ "id": "124", "t": "Notehead", "def": { "p": 2, "a": "" } }] },
-                        { "id": "125", "t": "Note", "def": { "time": { "num": 1, "den": 4 }, "abs": { "num": 1, "den": 1 }, "noteId": "n1_4" }, "children": [{ "id": "126", "t": "Notehead", "def": { "p": 1, "a": "" } }] },
-                        { "id": "127", "t": "Note", "def": { "time": { "num": 1, "den": 4 }, "abs": { "num": 5, "den": 4 }, "noteId": "n1_4" }, "children": [{ "id": "128", "t": "Notehead", "def": { "p": 3, "a": "" } }] },
-                        { "id": "129", "t": "Note", "def": { "time": { "num": 1, "den": 4 }, "abs": { "num": 3, "den": 2 }, "noteId": "n1_4" }, "children": [{ "id": "130", "t": "Notehead", "def": { "p": 5, "a": "" } }] }
+
                     ]
                 },
                 {
                     "id": "131", "t": "Voice", "def": { "stem": 2 },
                     "children": [
-                        { "id": "132", "t": "Note", "def": { "time": { "num": 1, "den": 1 }, "abs": { "num": 0, "den": 1 }, "noteId": "hidden", "rest": true, "hidden": true } },
-                        { "id": "133", "t": "Note", "def": { "time": { "num": 3, "den": 4 }, "abs": { "num": 1, "den": 1 }, "noteId": "hidden", "rest": true, "hidden": true } }
+                        { "id": "132", "t": "Note", "def": { "time": { "num": 1, "den": 1 }, "abs": { "num": 0, "den": 1 },
+                         "noteId": "hidden", "rest": true, "hidden": true } },
+                        { "id": "133", "t": "Note", "def": { "time": { "num": 3, "den": 4 }, "abs": { "num": 1, "den": 1 },
+                         "noteId": "hidden", "rest": true, "hidden": true } }
                     ]
                 },
                 { "id": "150", "t": "Meter" },
@@ -81,7 +90,10 @@ export class JmusicScoreViewComponent implements OnInit {
                 {
                     "id": "138", "t": "Voice", "def": { "stem": 1 },
                     "children": [
-                        { "id": "139", "t": "Note", "def": { "time": { "num": 1, "den": 1 }, "abs": { "num": 0, "den": 1 }, "noteId": "hidden", "rest": true, "hidden": true } }, { "id": "140", "t": "Note", "def": { "time": { "num": 3, "den": 4 }, "abs": { "num": 1, "den": 1 }, "noteId": "hidden", "rest": true, "hidden": true } }
+                        { "id": "139", "t": "Note", "def": { "time": { "num": 1, "den": 1 }, "abs": { "num": 0, "den": 1 },
+                        "noteId": "hidden", "rest": true, "hidden": true } }, { "id": "140", "t": "Note",
+                        "def": { "time": { "num": 3, "den": 4 }, "abs": { "num": 1, "den": 1 },
+                        "noteId": "hidden", "rest": true, "hidden": true } }
                     ]
                 },
                 { "id": "152", "t": "Meter" },
@@ -99,23 +111,22 @@ export class JmusicScoreViewComponent implements OnInit {
 //  private painter: CanvasView.CanvasQuickPainter;
 
   @Input() set memento(value: IMemento) {
-    try{
-    //console.log(value);
+    try {
+    // console.log(value);
 
-    var voiceMemento = value[0].mus;
-    //voiceMemento.children = 
+    const voiceMemento = value[0].mus;
     voiceMemento.children.reverse();
-    var staffMemento = {def:{}, t:"Staff", children:[{ "t": "Clef", "def": { "abs": { "num": 0, "den": 1 }, "clef": 1, "lin": 4, "tr": 0 } }, voiceMemento], id: '2'};
-    var scoreMemento = {def:{}, t:"Score", children:[staffMemento], id: '3' };
-    //var scoreMemento = this.mus;
-    //var score = new Model.ScoreElement(null);
-    //var score = Model.ScoreElement.createFromMemento(null, <any>scoreMemento);
-    var score = <ScoreElement>MusicElementFactory.recreateElement(null, <any>scoreMemento);
+    const staffMemento = {def: {}, t: "Staff", children: [
+        { "t": "Clef", "def": { "abs": { "num": 0, "den": 1 }, "clef": 1, "lin": 4, "tr": 0 } },
+        voiceMemento
+    ], id: '2'};
+    const scoreMemento = {def: {}, t: "Score", children: [staffMemento], id: '3' };
+    const score = <ScoreElement>MusicElementFactory.recreateElement(null, <any>scoreMemento);
 
     /*var staff = score.addStaff(Model.ClefDefinition.clefCAlto);
     var voice = staff.addVoice();
     var note = Model.Music.addNote(voice, Model.NoteType.Note, new Model.AbsoluteTime(1,4), "n1_4",  Model.TimeSpan.quarterNote);*/
-    var memento = score.getMemento(true);
+    const memento = score.getMemento(true);
     this.theScore = score;
     this.theScoreMemento = JSON.stringify(memento);
 
@@ -128,30 +139,29 @@ export class JmusicScoreViewComponent implements OnInit {
     //console.log(this.canvas.nativeElement);
     this.painter.paintOnCanvas(score, this.canvas.nativeElement);}
     catch(e1){
-      console.log(e1); 
+      console.log(e1);
     }*/
-  }
-  catch(e){
-    //console.log("Fejlede");
+  } catch (e) {
+    // console.log("Fejlede");
     this.theScore = null;
     this.theScoreMemento = "";
     this._memento = null;
   }
-};
+}
 
   get memento() {
     return this._memento;
-  };
-  
+  }
+
   @ViewChild('theCanvas') canvas: ElementRef;
 
-  
+
   ngOnInit() {
-    var score = new ScoreElement(null);
-    var staff = score.addStaff(ClefDefinition.clefCAlto);
-    var voice = staff.addVoice();
-    var note = Music.addNote(voice, NoteType.Note, new AbsoluteTime(1,4), "n1_4",  TimeSpan.quarterNote);
-    var memento = score.getMemento(true);
+    const score = new ScoreElement(null);
+    const staff = score.addStaff(ClefDefinition.clefCAlto);
+    const voice = staff.addVoice();
+    const note = Music.addNote(voice, NoteType.Note, new AbsoluteTime(1, 4), "n1_4",  TimeSpan.quarterNote);
+    const memento = score.getMemento(true);
     this.theScore = score;
     this.theScoreMemento = JSON.stringify(memento);
 
