2b5c7d1a7ed1c6191b09141ed664cebcd6cd5b33
John Ekberg <joek@adm.ku.dk>
2019-01-11 10:02:48 +0100

    tom jm-instruments

---
+++ jMusic/jm-instruments.ts
@@ -0,0 +1,34 @@
+
+
+/*
+Instrument-rank:
+
+lowNote
+highNote
+
+
+Difficult notes/intervals
+Impossible notes/intervals
+
+Multi-note: impossible combinations
+	Harpe: samme rodtone
+	strengeinstrumenter: umulige akkorder. lowNote kan være array [g d' a' e]. 
+
+Clefs
+
+Staff structure (1, 2 eller 3)
+
+For stort fingerspænd: strengeinstrumenter, klaver
+
+Transposing
+
+Muta: advarsel ved for hurtige skift mellem instrumenter eller stemninger (pauker, harper)
+
+Name
+
+Special markings (noteExpressions, staffExpressions)
+
+http://www.orchestralibrary.com/reftables/rang.html#
+https://dolmetsch.com/musictheory26.htm#soundingclefs
+
+*/
