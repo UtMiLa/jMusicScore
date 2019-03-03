4ffe21b6076f4ea94f42d012153e5b3b6fe32a69
UtMiLa <John@apus.dk>
2019-03-03 20:06:59 +0100

    lidt mere kode

--- jMusic/jm-stafflogic.ts
+++ jMusic/jm-stafflogic.ts
@@ -1,18 +1,19 @@
-import { IStaff, IMusicElement, ITimedChangeEvent } from "./model/jm-model-interfaces";
+import { IStaff, IMusicElement, ITimedChangeEvent, IEventVisitor, INoteHeadInfo, INoteInfo, INoteDecorationEventInfo, ILongDecorationEventInfo, ITextSyllableEventInfo, IBeamEventInfo, IBarEventInfo, IClefEventInfo, IMeterEventInfo, IKeyEventInfo, IStaffExpressionEventInfo, ISequence, IGlobalContext, IVoice, IScore } from "./model/jm-model-interfaces";
 import { AbsoluteTime, StaffContext, ClefDefinition, IMeterDefinition, TimeSpan, IKeyDefinition } from "./jm-music-basics";
+import { ContextEventVisitor } from "./model/jm-model-base";
 
 
 export interface IControlElement extends ITimedChangeEvent {
 }
 
 export class ControlElementRef {
-    element: IControlElement;
-    staves: IStaff[] = [];
+    constructor(public element: IControlElement, public relTime: TimeSpan, staff: IStaff) {
+    }
     next: ControlElementRef;
     prev: ControlElementRef;
 }
 
-export class ControlElementRepository {
+class ControlElementRepository extends ContextEventVisitor {
     elements: ControlElementRef[];
 
     invalidateRepository() {
@@ -22,8 +23,69 @@ export class ControlElementRepository {
     getElements(staff: IStaff, fromPosition: AbsoluteTime, toPosition: AbsoluteTime): IControlElement[] {
         return [];
     }
-}
 
+    /*visitNoteHeadInfo(head: INoteHeadInfo): void {
+        // Rien
+    }
+    visitNoteInfo(note: INoteInfo): void {
+        // Rien
+    }
+    visitNoteDecorationInfo(deco: INoteDecorationEventInfo): void {
+        // Rien
+    }*/
+    visitLongDecorationInfo(deco: ILongDecorationEventInfo): void {
+        
+    }
+    /*visitTextSyllableInfo(text: ITextSyllableEventInfo): void {
+        // Rien
+    }
+    visitBeamInfo(beam: IBeamEventInfo): void {
+        // Rien
+    }
+    visitBarInfo(bar: IBarEventInfo): void {
+        // Rien de rien
+    }*/
+    
+    private currentClef: ControlElementRef;
+    visitClefInfo(clef: IClefEventInfo): void {
+        const ref = new ControlElementRef(clef.source, clef.relTime, null);
+        ref.prev = this.currentClef;
+        if (this.currentClef) this.currentClef.next = ref;
+        this.elements.push(ref);
+    }
+
+    private currentMeter: ControlElementRef;
+    visitMeterInfo(meter: IMeterEventInfo): void {
+        const ref = new ControlElementRef(meter.source, meter.relTime, null);
+        ref.prev = this.currentMeter;
+        if (this.currentMeter) this.currentMeter.next = ref;
+        this.elements.push(ref);
+    }
+    private currentKey: ControlElementRef;
+    visitKeyInfo(key: IKeyEventInfo): void {
+        const ref = new ControlElementRef(key.source, key.relTime, null);
+        ref.prev = this.currentKey;
+        if (this.currentKey) this.currentKey.next = ref;
+        this.elements.push(ref);
+    }
+    visitStaffExpressionInfo(staffExpression: IStaffExpressionEventInfo): void {
+        
+    }
+    /*visitSequence(sequence: ISequence, globalContext: IGlobalContext): void {
+        
+    }
+    visitVoice(voice: IVoice): void {
+        
+    }
+    visitStaff(staff: IStaff): void {
+        
+    }
+    visitScore(score: IScore): void {
+        
+    }*/
+
+
+}
 /*
 
 Kontrolelementer er:
@@ -44,19 +106,20 @@ Case 3: Vi skal rendere takt T på staff S. Giv os en sorteret liste over alle e
 */
 
 export class ControlElementManager{
-    constructor(){ 
+    constructor(private score: IScore, private globalContext: IGlobalContext){ 
  
     }
 
     private scopeMeterGlobally: boolean = true;
     private scopeKeyGlobally: boolean = true;
 
-    private repository = new ControlElementRepository();
+    private repository = new ControlElementRepository(this.globalContext);
 
     private generateContext() {
         // if cached and not invalidated return cache
 
         // get alle control events from score recursively and map them to scope
+        this.score.visitAllEvents(this.repository, this.globalContext);
     }
 
     /**
@@ -86,3 +149,4 @@ export class ControlElementManager{
     }
 
 }
+
