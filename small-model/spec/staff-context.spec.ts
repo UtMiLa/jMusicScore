3cd9fc8bfcc63a3dd3df9c6cf9a027fba2c14be2
UtMiLa <John@apus.dk>
2020-06-02 18:22:05 +0200

    Clean project for development of simple model

---
+++ small-model/spec/staff-context.spec.ts
@@ -0,0 +1,19 @@
+
+describe("Generation of staffContext", () => {
+    it("should correctly register staff's start clef", () => {});
+    it("should correctly register staff's start key", () => {});
+    it("should correctly register section's start key", () => {});
+    it("should correctly register staff's start meter", () => {});
+    it("should correctly register section's start meter", () => {});
+    it("should correctly register clef changes in a staff", () => {});
+    it("should correctly register clef changes in a voice", () => {});
+    it("should correctly register clef changes in a sequence", () => {});
+    it("should correctly register key changes in a section", () => {});
+    it("should correctly register key changes in a staff", () => {});
+    it("should correctly register key changes in a voice", () => {});
+    it("should correctly register key changes in a sequence", () => {});
+    it("should correctly register meter changes in a section", () => {});
+    it("should correctly register meter changes in a staff", () => {});
+    it("should correctly register meter changes in a voice", () => {});
+    it("should correctly register meter changes in a sequence", () => {});
+});
